d5e78bcb3440604f1c343edbd526498c43af910b
Antenor <nanaosei452@gmail.com>
2023-02-03 10:45:05 +0000

    creating ui service

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,22 @@
 import { Component ,OnInit} from '@angular/core';
-
+import { UiService } from 'src/app/services/ui.service';
+import {Subscription} from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  title:string = 'Task Tracker'
+  showAddTask!:boolean;
+  subscription!:Subscription;
 
-title:string = 'Task Tracker'
+
+  constructor( private uiService:UiService = new UiService())
+  {}
 
 toggleAddTask(){
-  console.log('toggle');
+  this.uiService.toggleAddTask();
 
 }
 
